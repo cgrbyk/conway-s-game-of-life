af636e9c2a8f2a4bdfd2a280c9b780085c19202e
cgrbyk <cgrbyk@gmail.com>
2019-07-14 09:33:13 +0300

    çözünürlük ve thread düzenlemesi

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import threading
 #komşu sayısına göre ölüm veya yaşamı belirlemek için kullanılan matris
 isDeadArray=[[1,1,0,0,1,1,1,1,1],[1,1,1,0,1,1,1,1,1]]
 jen=0
+CellSize=5
+timer = None
 class Cell:
     def __init__(self, *args, **kwargs):
         self.isDead = 1
@@ -33,6 +35,7 @@ class Cell:
 #Cells matrisindeki her bir ücre için yaşam ve ölüm durumunun hesaplanması recursive yapıda
 def CellsCalc():
     global jen
+    global timer
     jen+=1
     for row in cells:
         for c in row:
@@ -44,7 +47,7 @@ def CellsCalc():
     timer.start() 
 
 
-column, row = 80, 60
+column, row = 160,120
 cells = []
 for i in range(column*row):
     cells.append(Cell())
@@ -64,7 +67,7 @@ for i in range(1, column-1):
         cells[i][j].isDead=random.randint(0,1)
 #pygame init ve ekran özelliklerinin verilmesi
 pygame.init()
-gameDisplay=pygame.display.set_mode((800,600))
+gameDisplay=pygame.display.set_mode((column*CellSize,row*CellSize))
 pygame.display.set_caption('Conways Game of Life')
 clock=pygame.time.Clock()
 gameDisplay.fill((255,255,255))
@@ -77,15 +80,15 @@ while(isCrashed != 12):
     #cells matrixindeki elemanları oyun alanına çiziyor her bir eleman 10x10 pikselden oluşuyor yaşayanlar siyah ölüler beyaz
     for i in range(0, column):
         for j in range(0, row):
-            pygame.draw.rect(gameDisplay,(cells[i][j].isDead*255,cells[i][j].isDead*255,cells[i][j].isDead*255),pygame.Rect(i*10,j*10,10,10))
+            pygame.draw.rect(gameDisplay,(cells[i][j].isDead*255,cells[i][j].isDead*255,cells[i][j].isDead*255),pygame.Rect(i*CellSize,j*CellSize,CellSize,CellSize))
     text = font.render('jenerasyon :'+str(jen), True, (0, 128, 0))
     gameDisplay.blit(text,(0,0))
     for event in pygame.event.get():
             isCrashed=event.type
+            print(event)
     pygame.display.update()
     #oyunu 60 fps'e sabitliyor
     clock.tick(60)
 
 pygame.quit()
-quit()
-
+timer.cancel()
